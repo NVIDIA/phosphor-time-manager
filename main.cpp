9f447d2ce73af4e20cb6d76adfc7b9868806bdf4
sushilkdubey@gmail.com <sushilkdubey>
2023-01-20 10:45:37 +0000

    coverity fix, Fixes nvbug https://nvbugswb.nvidia.com/NvBugs5/SWBug.aspx?bugid=3937204&cmtNo=

--- main.cpp
+++ main.cpp
@@ -5,34 +5,46 @@
 
 #include <sdbusplus/bus.hpp>
 
+#include <iostream>
+#include <exception>
+
 int main()
 {
-    auto bus = sdbusplus::bus::new_default();
-    sd_event* event = nullptr;
+    try
+    {
+        auto bus = sdbusplus::bus::new_default();
+        sd_event* event = nullptr;
 
-    auto eventDeleter = [](sd_event* e) { e = sd_event_unref(e); };
-    using SdEvent = std::unique_ptr<sd_event, decltype(eventDeleter)>;
+        auto eventDeleter = [](sd_event* e) { e = sd_event_unref(e); };
+        using SdEvent = std::unique_ptr<sd_event, decltype(eventDeleter)>;
 
-    // acquire a reference to the default event loop
-    sd_event_default(&event);
-    SdEvent sdEvent{event, eventDeleter};
-    event = nullptr;
+        // acquire a reference to the default event loop
+        sd_event_default(&event);
+        SdEvent sdEvent{event, eventDeleter};
+        event = nullptr;
 
-    // attach bus to this event loop
-    bus.attach_event(sdEvent.get(), SD_EVENT_PRIORITY_NORMAL);
+        // attach bus to this event loop
+        bus.attach_event(sdEvent.get(), SD_EVENT_PRIORITY_NORMAL);
 
-    // Add sdbusplus ObjectManager
-    sdbusplus::server::manager::manager bmcEpochObjManager(bus, OBJPATH_BMC);
+        // Add sdbusplus ObjectManager
+        sdbusplus::server::manager::manager bmcEpochObjManager(bus,
+                                                               OBJPATH_BMC);
 
-    phosphor::time::BmcEpoch bmc(bus, OBJPATH_BMC);
-    phosphor::time::Manager manager(bus);
+        phosphor::time::BmcEpoch bmc(bus, OBJPATH_BMC);
+        phosphor::time::Manager manager(bus);
 
-    bus.request_name(BUSNAME);
+        bus.request_name(BUSNAME);
 
-    // Start event loop for all sd-bus events and timer event
-    sd_event_loop(bus.get_event());
+        // Start event loop for all sd-bus events and timer event
+        sd_event_loop(bus.get_event());
 
-    bus.detach_event();
+        bus.detach_event();
 
-    return 0;
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << __func__ << e.what() << std::endl;
+        return -1;
+    }
 }
