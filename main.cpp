openbmc/phosphor-user-manager

sdbusplus: use shorter type aliases
The sdbusplus headers provide shortened aliases for many types.
Switch to using them to provide better code clarity and shorter
lines.  Possible replacements are for:
  * bus_t
  * exception_t
  * manager_t
  * match_t
  * message_t
  * object_t
  * slot_t

Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: I19e094a2a99893627dbb82b615f630b87ad90fdd

phosphor-ldap-config/main.cpp
@@ -28,7 +28,7 @@ int main(int /*argc*/, char** /*argv*/)
     auto bus = sdbusplus::bus::new_default();
 
     // Add sdbusplus ObjectManager for the 'root' path of the LDAP config.
-    sdbusplus::server::manager::manager objManager(bus, LDAP_CONFIG_ROOT);
+    sdbusplus::server::manager_t objManager(bus, LDAP_CONFIG_ROOT);
 
     phosphor::ldap::ConfigMgr mgr(bus, LDAP_CONFIG_ROOT, LDAP_CONFIG_FILE,
                                   LDAP_CONF_PERSIST_PATH, TLS_CACERT_PATH,

NVIDIA/phosphor-time-manager

Change-Id: I9cedddf4dc9b224ecbc2f68e6b00c0ab1885a826

main.cpp
@@ -27,8 +27,7 @@ int main()
         bus.attach_event(sdEvent.get(), SD_EVENT_PRIORITY_NORMAL);
 
         // Add sdbusplus ObjectManager
-        sdbusplus::server::manager::manager bmcEpochObjManager(bus,
-                                                               OBJPATH_BMC);
+        sdbusplus::server::manager_t bmcEpochObjManager(bus, OBJPATH_BMC);
 
         phosphor::time::BmcEpoch bmc(bus, OBJPATH_BMC);
         phosphor::time::Manager manager(bus);